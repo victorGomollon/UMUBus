9e13a10fe0f9c34b9d8463f91658207921ae3dd2
Victor Gomollon Martos <victor.gomollon@ticarum.es>
2023-02-14 12:58:24 +0100

    Primer commit

---
+++ src/main/java/es/um/atica/bus/umubus/UmuBusApplication.java
@@ -0,0 +1,13 @@
+package es.um.atica.bus.umubus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmuBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmuBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/um/atica/bus/umubus/UmuBusApplicationTests.java
@@ -0,0 +1,13 @@
+package es.um.atica.bus.umubus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmuBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
