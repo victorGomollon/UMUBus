4affe8077f8f99cbd97d55345c0260f9b0ff7419
wolfo <wolfo@DESKTOP-8426N00>
2023-04-17 09:27:29 +0200

    Añadido Cambio

--- umubus-lib/src/main/java/es/um/atica/umubus_lib/adapters/queue/RabbitConsumerEvent.java
+++ umubus-lib/src/main/java/es/um/atica/umubus_lib/adapters/queue/RabbitConsumerEvent.java
@@ -8,13 +8,13 @@ import org.springframework.cloud.stream.binder.rabbit.properties.RabbitConsumerP
 import es.um.atica.umubus_lib.domain.events.Event;
 import es.um.atica.umubus_lib.domain.queue.ConsumerEvent;
 
-public class RabbitConsumerEvent<T extends Event> //extends ExtendedConsumerProperties<RabbitConsumerProperties> 
+public class RabbitConsumerEvent<T extends Event> extends ExtendedConsumerProperties<RabbitConsumerProperties> 
 								 implements ConsumerEvent<Event>{
 
-//	public RabbitConsumerEvent(RabbitConsumerProperties extension) {
-//		super(extension);
-//		// TODO Auto-generated constructor stub
-//	}
+	public RabbitConsumerEvent(RabbitConsumerProperties extension) {
+		super(extension);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void accept(Event t) {
@@ -25,10 +25,9 @@ public class RabbitConsumerEvent<T extends Event> //extends ExtendedConsumerProp
 	//En esta clase tenemos que rellenar el properties de bindingRoutingKey para el nuevo exchange para cada consumer
 	//La idea es que al cargar el contexto del consumidor cree automaticamente el binding con todas sus propiedades....
 	private void setBindingProperties(Event event) {
-		ExtendedConsumerProperties<RabbitConsumerProperties> properties = new ExtendedConsumerProperties<RabbitConsumerProperties> (new RabbitConsumerProperties());
-		properties.getExtension().setExchangeType(ExchangeTypes.TOPIC);
-		properties.getExtension().setBindingRoutingKey(event.getClass().getName());
-		properties.getExtension().setBindingRoutingKeyDelimiter(",");
-		properties.getExtension().setQueueNameGroupOnly(true);
+		this.getExtension().setExchangeType(ExchangeTypes.TOPIC);
+		this.getExtension().setBindingRoutingKey(event.getClass().getName());
+		this.getExtension().setBindingRoutingKeyDelimiter(",");
+		this.getExtension().setQueueNameGroupOnly(true);
 	}
 }
